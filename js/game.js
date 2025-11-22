5dc6bd08b018fd0f14b520ae0491f30346dd2742
PC_ALLAN\allan <allan.jf@yahoo.com>
2025-11-22 15:15:58 -0300

    Commit com tudo alinhado

--- js/game.js
+++ js/game.js
@@ -3,13 +3,22 @@ let bgLayers = [];
 let score = 0;
 let gameOver = false;
 
+let bgContainer, gameContainer, uiContainer;
+
 function init() {
     const canvas = document.getElementById("gameCanvas");
     stage = new createjs.Stage(canvas);
 
+    bgContainer = new createjs.Container();
+    gameContainer = new createjs.Container();
+    uiContainer = new createjs.Container();
+
+    stage.addChild(bgContainer, gameContainer, uiContainer);
+
     loadBackground();
-    player = new Player(stage);
-    obstacleManager = new ObstacleManager(stage);
+
+    player = new Player(gameContainer);
+    obstacleManager = new ObstacleManager(gameContainer);
 
     createjs.Ticker.framerate = 60;
     createjs.Ticker.on("tick", update);
@@ -19,59 +28,14 @@ function init() {
     });
 }
 
-// function loadBackground() {
-//     const layers = [
-//         // '0_ Background.png',
-//         // '1_ Trees Background.png',
-//         // '2_ Trees.png',
-//         // '3_ Trees.png',
-//         // '4_ Trees.png',
-//         'Tile (13).png',
-//         //'5_ Floor Platform.png'
-//     ];
-
-//     const baseSpeeds = [1];
-
-//     layers.forEach((filename, i) => {
-//         const img = new Image();
-//         img.src = encodeURI('assets/background/' + filename);
-
-//         img.onload = () => {
-//             const bmp1 = new createjs.Bitmap(img);
-//             const bmp2 = new createjs.Bitmap(img);
-//             bmp1.scaleX = 0.5;
-//             bmp1.scaleY = 0.5;
-//             bmp2.scaleX = 0.5;
-//             bmp2.scaleY = 0.5;
-
-//             // alinhamento exato no chão
-//             const groundY = 570;
-//             const tileHeight = img.height;
-//             const yPos = groundY - tileHeight;
-
-//             bmp1.y = bmp2.y = yPos;
-
-//             bmp1.x = 0;
-//             bmp2.x = img.width;
-
-//             stage.addChild(bmp1, bmp2);
-
-//             bgLayers.push({
-//                 bmp1,
-//                 bmp2,
-//                 speed: 8,
-//                 width: img.width
-//             });
-//         };
-//     });
-// }
 
 function loadBackground() {
-    const filename = "Tile (13).png";
-    const img = new Image();
-    img.src = encodeURI("assets/background/" + filename);
-
-    img.onload = () => {
+    const filenames = ["fundo.png", "chao.png"];
+    
+    filenames.forEach(file => {
+        const img = new Image();
+        img.src = encodeURI("assets/background/" + file);
+        img.onload = () => {
         const scale = 0.5;
 
         const bmp1 = new createjs.Bitmap(img);
@@ -85,7 +49,7 @@ function loadBackground() {
         const scaledHeight = img.height * scale;
 
         // altura onde o chão deve ficar
-        const groundY = 513
+        const groundY = 502
 
         // posiciona certinho encostado no chão
         const yPos = groundY - scaledHeight;
@@ -103,19 +67,12 @@ function loadBackground() {
             speed: 8,
             width: scaledWidth
         });
-    };
+
+        bgContainer.addChild(bmp1, bmp2);
+
+        };
+    })
 }
-
-// function updateBackground() {
-//     bgLayers.forEach(layer => {
-//         layer.bmp1.x -= layer.speed;
-//         layer.bmp2.x -= layer.speed;
-
-//         const w = layer.width || 960;
-//         if (layer.bmp1.x < -w) layer.bmp1.x = layer.bmp2.x + w;
-//         if (layer.bmp2.x < -w) layer.bmp2.x = layer.bmp1.x + w;
-//     });
-// }
 
 function updateBackground() {
     bgLayers.forEach(layer => {
